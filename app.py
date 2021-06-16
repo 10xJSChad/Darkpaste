d1e2481bc5cc6911bb8f7b8c90196f45f7053174
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2021-06-16 13:35:15 +0200

    Major update
    Fixed paste formatting
    Added unlisted and private pastes
    Removed "support" for html/css/js in pastes
    Other QoL stuff

--- app.py
+++ app.py
@@ -9,7 +9,14 @@ app = Flask(__name__)
 darkpaste = AnaDatabase("Darkpaste")
 
 #darkpaste.create("users", ["user", "pass", "token", "ip"])
-#darkpaste.create("pastes", ["url", "name", "content", "number", "poster", "private"])
+#darkpaste.create("pastes", ["url", "name", "content", "number", "poster", "type"])
+#darkpaste.save()
+
+def resetDb():
+ darkpaste.create("users", ["user", "pass", "token", "ip"])
+ darkpaste.create("pastes", ["url", "name", "content", "number", "poster", "type"])
+ darkpaste.save()
+ exit()
 
 darkpaste.load()
 
@@ -34,6 +41,16 @@ def checkLogged():
  if len(result) == 0: return False
  if result[0][0] == ip: return True
  return False
+
+def checkLoggedGetUser():
+ token = request.cookies.get('token')
+ ip = request.cookies.get('ip')
+ if token == None: return False, "Guest"
+ if ip == None: return False, "Guest"
+ result = darkpaste.getWhere("users", "token=" + token, "select=user,ip")
+ if len(result) == 0: return False, "Guest"
+ if result[0][1] == ip: return True, result[0][0]
+ return False, "Guest"
 
 def verifyLogin(username, password):
  global darkpaste
@@ -69,17 +86,51 @@ def login():
 @app.route("/pastes/<url>")
 def paste(url):
     global darkpaste
-    content = (darkpaste.getWhere("pastes", "url=" + url, "select=name,content"))
+    viewingUser = checkLoggedGetUser()[1]
+    isOwner = False
+    
+    content = (darkpaste.getWhere("pastes", "url=" + url, "select=name,content,poster,type"))
+    privateContent = [["Private Paste", "Only the owner of this paste can view it", content[0][2]]]
+    if content[0][2] == viewingUser: isOwner = True
+    
+    if (content[0][3] == "private" and isOwner == False):
+     return render_template("paste/paste.html", content=privateContent)
+ 
     return render_template("paste/paste.html", content=content)
 
 @app.route("/user/<url>")
 def user(url):
     global darkpaste
-    urls = (darkpaste.getWhere("pastes", "poster=" + url, "select=url"))
-    titles = (darkpaste.getWhere("pastes", "poster=" + url, "select=name"))
-    posters = (darkpaste.getWhere("pastes", "poster=" + url, "select=poster"))
-    if len(urls) < 11: length = len(urls)
-    else: length = 11
+    viewingUser = checkLoggedGetUser()[1]
+    urls = (darkpaste.getWhere("pastes", "poster=" + url, "select=url,type"))
+    titles = (darkpaste.getWhere("pastes", "poster=" + url, "select=name,type"))
+    posters = (darkpaste.getWhere("pastes", "poster=" + url, "select=poster,type"))
+    
+    newUrls = []
+    newTitles = []
+    newPosters = []
+
+    if viewingUser != url:
+     for x in urls: 
+         if x[1] == "public": newUrls.append(x[0])   
+     for x in titles: 
+         if x[1] == "public": newTitles.append(x[0])       
+     for x in posters: 
+         if x[1] == "public": newPosters.append(x[0]) 
+         
+    else:
+     for x in urls: 
+         newUrls.append(x[0])   
+     for x in titles: 
+         newTitles.append(x[0] + " (" + x[1] + ")")
+     for x in posters: 
+         newPosters.append(x[0])
+     
+    urls = newUrls
+    titles = newTitles
+    posters = newPosters   
+    
+    length = len(urls)
     return render_template('user/user.html', titles=titles, urls=urls, length=length, posters=posters)
 
 @app.route("/")
@@ -87,9 +138,9 @@ def index():
     global darkpaste
     username = ""
     if checkLogged(): username = getUser()   
-    urls = (darkpaste.getWhere("pastes", "number=0", "select=url"))
-    titles = (darkpaste.getWhere("pastes", "number=0", "select=name"))
-    posters = (darkpaste.getWhere("pastes", "number=0", "select=poster"))
+    urls = (darkpaste.getWhere("pastes", "type=public", "select=url"))
+    titles = (darkpaste.getWhere("pastes", "type=public", "select=name"))
+    posters = (darkpaste.getWhere("pastes", "type=public", "select=poster"))
     if len(urls) < 11: length = len(urls)
     else: length = 11
     return render_template('main.html', titles=titles, urls=urls, length=length, posters=posters, username=username)
@@ -103,8 +154,14 @@ def pastePage():
 def create_paste():
     if not checkLogged(): return render_template('notlogged.html')
     username = getUser()
-    title, content = request.values.get('title'), request.values.get('content') 
-    darkpaste.enter("pastes", [generateUrl(), title, content, 0, username, "false"])
+    title, content, pasteType = request.values.get('title'), request.values.get('content'), request.values.get('pasteType')
+    fixedPasteType = "public"
+    
+    if pasteType == "public": fixedPasteType = pasteType
+    if pasteType == "unlisted": fixedPasteType = pasteType
+    if pasteType == "private": fixedPasteType = pasteType
+    
+    darkpaste.enter("pastes", [generateUrl(), title, content, 0, username, fixedPasteType])
     darkpaste.save()
     index()
     return index()
